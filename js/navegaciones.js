229e8a7c48bd2e0a52934906336b0f1ae2084d0c
Memin <villagran_gg@hotmail.com>
2016-08-14 00:52:47 -0500

    presentado

---
+++ js/navegaciones.js
@@ -0,0 +1,29 @@
+	$(document).ready(function() {
+		$("#introduccion").click(function(event) {
+			$("#contenedor").load('introduccion.html');
+		});
+	});
+
+	$(document).ready(function() {
+		$("#tipos").click(function(event) {
+			$("#contenedor").load('tipos.html');
+		});
+	});
+
+	$(document).ready(function() {
+		$("#trinomio").click(function(event) {
+			$("#contenedor").load('trinomio.html');
+		});
+	});
+
+	$(document).ready(function() {
+		$("#youtube").click(function(event) {
+			$("#contenedor").load('espiritu.html');
+		});
+	});
+
+	$(document).ready(function() {
+		$("#conceptos").click(function(event) {
+			$("#contenedor").load('conceptos.html');
+		});
+	});
